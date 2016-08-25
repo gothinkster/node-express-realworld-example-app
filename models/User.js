6ed76fb66fcccb4a0fafdef2a5413374e2f9cb49
Albert Pai <albert@pai.email>
2016-08-25 16:10:58 -0700

    Add following

--- models/User.js
+++ models/User.js
@@ -52,7 +52,7 @@ UserSchema.methods.toProfileJSONFor = function(user){
     username: this.username,
     bio: this.bio,
     image: this.image || 'https://static.productionready.io/images/smiley-cyrus.jpg',
-    following: false
+    following: user ? user.isFollowing(this._id) : false
   };
 };
 
@@ -72,6 +72,25 @@ UserSchema.methods.unfavorite = function(id){
 UserSchema.methods.isFavorite = function(id){
   return this.favorites.some(function(favoriteId){
     return favoriteId.toString() === id.toString();
+  });
+};
+
+UserSchema.methods.follow = function(id){
+  if(this.favorites.indexOf(id) === -1){
+    this.following.push(id);
+  }
+
+  return this.save();
+};
+
+UserSchema.methods.unfollow = function(id){
+  this.following.remove(id);
+  return this.save();
+};
+
+UserSchema.methods.isFollowing = function(id){
+  return this.following.some(function(followId){
+    return followId.toString() === id.toString();
   });
 };
 
--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -26,4 +26,28 @@ router.get('/:username', auth.optional, function(req, res, next){
   }
 });
 
+router.post('/:username/follow', auth.required, function(req, res, next){
+  var profileId = req.profile._id;
+
+  User.findById(req.payload.id).then(function(user){
+    if (!user) { return res.sendStatus(401); }
+
+    return user.follow(profileId).then(function(){
+      return res.json({profile: req.profile.toProfileJSONFor(user)});
+    });
+  }).catch(next);
+});
+
+router.delete('/:username/follow', auth.required, function(req, res, next){
+  var profileId = req.profile._id;
+
+  User.findById(req.payload.id).then(function(user){
+    if (!user) { return res.sendStatus(401); }
+
+    return user.unfollow(profileId).then(function(){
+      return res.json({profile: req.profile.toProfileJSONFor(user)});
+    });
+  }).catch(next);
+});
+
 module.exports = router;
