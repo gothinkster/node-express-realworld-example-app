8063559c8047e1f3cc0bb84dc70c02fc493c9a50
Albert Pai <albert@pai.email>
2016-08-25 16:11:25 -0700

    Create tags endpoint

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,7 @@ var router = require('express').Router();
 router.use('/', require('./users'));
 router.use('/profiles', require('./profiles'));
 router.use('/articles', require('./articles'));
+router.use('/tags', require('./tags'));
 
 router.use(function(err, req, res, next){
   if(err.name === 'ValidationError'){
