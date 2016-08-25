99bba4c7850b9ad353caa72b52e027bc8a3dc1a2
Albert Pai <albert@pai.email>
2016-08-25 14:15:46 -0700

    Create profiles endpoint

--- models/User.js
+++ models/User.js
@@ -47,4 +47,13 @@ UserSchema.methods.toAuthJSON = function(){
   };
 };
 
+UserSchema.methods.toProfileJSONFor = function(user){
+  return {
+    username: this.username,
+    bio: this.bio,
+    image: this.image || 'https://static.productionready.io/images/smiley-cyrus.jpg',
+    following: false
+  };
+};
+
 mongoose.model('User', UserSchema);
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,7 @@
 var router = require('express').Router();
 
 router.use('/', require('./users'));
+router.use('/profiles', require('./profiles'));
 
 router.use(function(err, req, res, next){
   if(err.name === 'ValidationError'){
