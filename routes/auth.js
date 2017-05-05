45357f113ac5ced3c4591beec1b1450f7432d0fc
Albert Pai <iamalbertpai@gmail.com>
2017-05-05 09:16:54 -0700

    Fix Token/Bearer check (#29)

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,8 @@ var jwt = require('express-jwt');
 var secret = require('../config').secret;
 
 function getTokenFromHeader(req){
-  if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Token') ||
-  (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+  if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Token' ||
+      req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
     return req.headers.authorization.split(' ')[1];
   }
 
