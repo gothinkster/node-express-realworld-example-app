491984353f99780a49b5e050d4fb1f7d7a0994d7
Rishi Goomar <rishig92@gmail.com>
2017-12-28 22:24:09 -0600

    forgot parens

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,8 @@ var secret = require('../config').secret;
 
 function getTokenFromHeader(req){
   if (req.headers.authorization && 
-      req.headers.authorization.split(' ')[0] === 'Token' ||
-      req.headers.authorization.split(' ')[0] === 'Bearer') {
+       (req.headers.authorization.split(' ')[0] === 'Token' || 
+        req.headers.authorization.split(' ')[0] === 'Bearer')){
     return req.headers.authorization.split(' ')[1];
   }
 
