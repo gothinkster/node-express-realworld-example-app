91ea765594cf241e4a98cca79945c00527819e93
Michael <Michaelkerl16@gmail.com>
2018-07-11 23:19:57 -0600

    Throw Error On Empty Password Field (#39)

--- models/User.js
+++ models/User.js
@@ -29,7 +29,7 @@ UserSchema.methods.validPassword = function(password) {
 UserSchema.methods.setPassword = function(password){
   this.salt = crypto.randomBytes(16).toString('hex');
   this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
-  this.password = password !== '' ? true : null;
+  this.password = (password !== '') ? true : null;
 };
 
 UserSchema.methods.generateJWT = function() {
