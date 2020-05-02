61cb43e64631e26bdc3a7d69e3e5cd603b4b1155
Kevin Lamping <klamping@gmail.com>
2020-05-02 10:07:53 -0500

    delete db on import

--- mongoImport.sh
+++ mongoImport.sh
@@ -1,3 +1,4 @@
+mongo conduit --eval "db.dropDatabase();"
 mongoimport -d conduit -c articles --file Articles.json --mode upsert
 mongoimport -d conduit -c users --file Users.json --mode upsert
 mongoimport -d conduit -c comments --file Comments.json --mode upsert
